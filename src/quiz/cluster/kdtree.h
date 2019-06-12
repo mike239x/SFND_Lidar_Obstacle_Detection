aabd3bef1790bb3c26055b09d6cad255b345ba94
mikhail.lezhnin <mikhail.lezhnin@esrlabs.com>
2019-06-12 17:27:23 +0200

    Implement search function for KDTree

--- src/quiz/cluster/kdtree.h
+++ src/quiz/cluster/kdtree.h
@@ -80,13 +80,50 @@ struct KdTree
 	// return a list of point ids in the tree that are within distance of target
 	std::vector<int> search(std::vector<float> target, float distanceTol)
 	{
+        auto dist = [](std::vector<float> a, std::vector<float> b) -> float
+        {
+            float d = 0.0;
+            for (size_t i = 0; i < a.size(); ++i)
+                d += (a[i]-b[i])*(a[i]-b[i]);
+            return std::sqrt(d);
+        };
 		std::vector<int> ids;
+        std::vector<Node*> branches;
+        std::vector<int> split_types;
+        branches.push_back(root);
+        split_types.push_back(0);
+        while (branches.size())
+        {
+            Node* node = branches.back();
+            branches.pop_back();
+            int split_type = split_types.back();
+            split_types.pop_back();
+            {
+                float d = dist(node->point, target);
+                if (d < distanceTol)
+                    ids.push_back(node->id);
+            }
+            {
+                float d = node->point[split_type] - target[split_type];
+                // positive d means target is to the left of the current point
+                int next_split_type = (split_type + 1) % target.size();
+                if (d < distanceTol && node->right != nullptr)
+                {
+                    // add right branch to the search
+                    branches.push_back(node->right);
+                    split_types.push_back(next_split_type);
+                }
+                if (d > -distanceTol && node->left != nullptr)
+                {
+                    // add left branch to the search
+                    branches.push_back(node->left);
+                    split_types.push_back(next_split_type);
+                }
+            }
+        }
 		return ids;
 	}
 	
 
 };
-
-
-
 
