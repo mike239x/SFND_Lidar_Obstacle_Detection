cd9210b55659114cdc5b3b3ee87ecbb43a751625
mikhail.lezhnin <mikhail.lezhnin@esrlabs.com>
2019-06-12 16:42:57 +0200

    Add hand-made KDTree

--- src/quiz/cluster/cluster.cpp
+++ src/quiz/cluster/cluster.cpp
@@ -18,7 +18,7 @@ pcl::visualization::PCLVisualizer::Ptr initScene(Box window, int zoom)
   	viewer->setCameraPosition(0, 0, zoom, 0, 1, 0);
   	viewer->addCoordinateSystem (1.0);
 
-  	viewer->addCube(window.x_min, window.x_max, window.y_min, window.y_max, 0, 0, 1, 1, 1, "window");
+    // viewer->addCube(window.x_min, window.x_max, window.y_min, window.y_max, 0, 0, 1, 1, 1, "window");
   	return viewer;
 }
 
@@ -100,8 +100,7 @@ int main ()
 	pcl::visualization::PCLVisualizer::Ptr viewer = initScene(window, 25);
 
 	// Create data
-	std::vector<std::vector<float>> points = { {-6.2,7}, {-6.3,8.4}, {-5.2,7.1}, {-5.7,6.3}, {7.2,6.1}, {8.0,5.3}, {7.2,7.1}, {0.2,-7.1}, {1.7,-6.9}, {-1.2,-7.2}, {2.2,-8.9} };
-	//std::vector<std::vector<float>> points = { {-6.2,7}, {-6.3,8.4}, {-5.2,7.1}, {-5.7,6.3} };
+    std::vector<std::vector<float>> points = { {-6.2,7}, {-6.3,8.4}, {-5.2,7.1}, {-5.7,6.3}, {7.2,6.1}, {8.0,5.3}, {7.2,7.1}, {0.2,-7.1}, {1.7,-6.9}, {-1.2,-7.2}, {2.2,-8.9} };
 	pcl::PointCloud<pcl::PointXYZ>::Ptr cloud = CreateData(points);
 
 	KdTree* tree = new KdTree;
--- src/quiz/cluster/kdtree.h
+++ src/quiz/cluster/kdtree.h
@@ -27,9 +27,54 @@ struct KdTree
 
 	void insert(std::vector<float> point, int id)
 	{
-		// TODO: Fill in this function to insert a new point into the tree
+		// DONE: Fill in this function to insert a new point into the tree
 		// the function should create a new node and place correctly with in the root 
-
+        if (root == nullptr)
+        {
+            root = new Node(point, id);
+        }
+        else
+        {
+            Node* cur = root;
+            Node* nxt = cur;
+            bool vert = true; // split vertically
+            while (true)
+            {
+                bool left;
+                if (vert)
+                    if (point[0] <= cur->point[0])
+                    {
+                        nxt = cur->left;
+                        left = true;
+                    }
+                    else
+                    {
+                        nxt = cur->right;
+                        left = false;
+                    }
+                else
+                    if (point[1] <= cur->point[1])
+                    {
+                        nxt = cur->left;
+                        left = true;
+                    }
+                    else
+                    {
+                        nxt = cur->right;
+                        left = false;
+                    }
+                if (nxt == nullptr)
+                {
+                    if (left)
+                        cur->left = new Node(point, id);
+                    else
+                        cur->right = new Node(point, id);
+                    break;
+                }
+                cur = nxt;
+                vert = !vert;
+            }
+        }
 	}
 
 	// return a list of point ids in the tree that are within distance of target
